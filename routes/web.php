6a1ff655a3cebc59d34f33af6bd8e80acf4a26c9
Larionov-Yurii <larionov.yurii.work@gmail.com>
2023-02-15 14:39:58 +0200

    Updating the routes file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserRegisterController;
+use App\Http\Controllers\UserLoginController;
+use App\Http\Controllers\EventLinkController;
+use App\Http\Controllers\EventNumbersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::name('user.')->group(function () {
+
+    // Start page with registration and redirect to the page that contains a button with a unique link
+    Route::get('/', [UserRegisterController::class, 'startPage'])->name('start-registration');
+
+    // User creation
+    Route::post('/user-creation', [UserRegisterController::class, 'createUser'])->name('sign-up');
+
+    // Login page with login and redirect to the page that contains a button with a unique link
+    Route::get('/login-page', [UserLoginController::class, 'loginPage'])->name('start-login');
+
+    // Logging in User
+    Route::post('/user-login', [UserLoginController::class, 'userLogin'])->name('sign-in');
+
+    Route::middleware('auth')->group(function () {
+        // Intermediate page that has a button to go to special page A
+        Route::get('/transition-page', [EventLinkController::class, 'transitionPage'])->name('link-page');
+
+        // Transition to page A with a unique link button
+        Route::get('/transition-page/transitioning', [EventLinkController::class, 'transitionToPageA'])->name('transition-via-click');
+
+        // Special page A
+        Route::get('/special-page-a/{token}', [EventNumbersController::class, 'eventPage'])->name('events-page');
+
+        // Random number generation
+        Route::post('/special-page-a/start-event', [EventNumbersController::class, 'imFeelingLucky'])->name('post-event');
+
+        // Getting information about the last three results of button clicks imFeelingLucky
+        Route::get('/get-history', [EventNumbersController::class, 'getHistory'])->name('get-history');
+
+        // Creating a new unique link
+        Route::post('/special-page-a/create-link', [EventNumbersController::class, 'createUniqueLink'])->name('create-link');
+
+        // Deactivating the current link
+        Route::post('/special-page-a/delete-link', [EventNumbersController::class, 'deactivateUniqueLink'])->name('delete-link');
+
+        // Logout
+        Route::post('/logout', [EventNumbersController::class, 'userLogout'])->name('logout');
+    });
 });
